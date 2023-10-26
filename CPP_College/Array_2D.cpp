419553a9a3f0ccc890a85ff4f22bb64b2f673fe3
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-10-26 21:18:35 +0530

    Create Array 2D in heap

--- CPP_College/Array_2D.cpp
+++ CPP_College/Array_2D.cpp
@@ -7,6 +7,7 @@
 //4 Columns
 // In Stack
 
+#include <cstdlib>
 int Arr2D [3][4] = {{1,2,3,4} , { 5,6,7,8}, {9,10,11,12}};
 int Arr1D [12] = {1,2,3,4 , 5,6,7,8, 9,10,11,12} ;
 
@@ -22,6 +23,33 @@ In C, you can pass arrays as arguments to functions,
 but you can't directly return arrays from functions.
 
 **/
+
+
+//Declaration of a 2d Array With
+//3 Rows
+//4 Columns
+// In Heap
+
+int main ()
+{
+
+
+
+    int NumOfRows;
+    int NumOfColumns;
+
+    int* Arr2D_Heap [NumOfRows];
+
+
+    for ( int i = 0 ; i < NumOfRows ; i++ )
+    {
+        int*Row = (int*) malloc(NumOfColumns*sizeof(int));
+        Arr2D_Heap[i] = Row;
+        
+    }
+}
+
+
 
 
 
--- CPP_College/CPP_College.cpp
+++ CPP_College/CPP_College.cpp
@@ -1,6 +1,9 @@
+#include <iostream>
+
+
+
 
 int main(int argc, char* argv[])
 {
-    
     return 0;
 }
