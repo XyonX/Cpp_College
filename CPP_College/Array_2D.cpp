c2ab8030f8a0a6625532d286c363c3a15bfc904b
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-10-26 21:08:30 +0530

    Create Array_2D in stack

---
+++ CPP_College/Array_2D.cpp
@@ -0,0 +1,27 @@
+﻿
+
+
+
+//Declaration of a 2d Array With
+//3 Rows
+//4 Columns
+// In Stack
+
+int Arr2D [3][4] = {{1,2,3,4} , { 5,6,7,8}, {9,10,11,12}};
+int Arr1D [12] = {1,2,3,4 , 5,6,7,8, 9,10,11,12} ;
+
+int Get_Element_By_ID ( int Row , int Column , int TotalRowNumber  ,int arr [] )
+{
+    int index = Column*TotalRowNumber + Row ; 
+    return arr [index];
+    
+}
+
+/**
+In C, you can pass arrays as arguments to functions,
+but you can't directly return arrays from functions.
+
+**/
+
+
+
