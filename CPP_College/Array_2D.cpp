c38d4a0e47054264f4e484b10883ba3b252767db
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-10-27 11:14:53 +0530

    Added 2d Array iteration

--- CPP_College/Array_2D.cpp
+++ CPP_College/Array_2D.cpp
@@ -7,6 +7,7 @@
 //4 Columns
 // In Stack
 
+#include <cstdio>
 #include <cstdlib>
 int Arr2D [3][4] = {{1,2,3,4} , { 5,6,7,8}, {9,10,11,12}};
 int Arr1D [12] = {1,2,3,4 , 5,6,7,8, 9,10,11,12} ;
@@ -16,6 +17,18 @@ int Get_Element_By_ID ( int Row , int Column , int TotalRowNumber  ,int arr [] )
     int index = Column*TotalRowNumber + Row ; 
     return arr [index];
     
+}
+
+void Iterating_All_The_Elements(int*Array [] , int Row , int Columns)
+{
+    for(int i =0 ; i < Row ; i++)
+    {
+
+        for ( int j =0; j< Columns;j++)
+        {
+            int Element = Array[i][j];
+        }
+    }
 }
 
 /**
@@ -35,10 +48,10 @@ int main ()
 
 
 
-    int NumOfRows;
-    int NumOfColumns;
+    int NumOfRows=3;
+    int NumOfColumns=4;
 
-    int* Arr2D_Heap [NumOfRows];
+    int* Arr2D_Heap [3];
 
 
     for ( int i = 0 ; i < NumOfRows ; i++ )
@@ -46,6 +59,39 @@ int main ()
         int*Row = (int*) malloc(NumOfColumns*sizeof(int));
         Arr2D_Heap[i] = Row;
         
+    }
+
+    int*Array_2D [3];
+
+    //Creating the actual array
+    for (int i = 0 ; i<3 ; i++)
+    {
+        Array_2D[i] = (int*) malloc(4*sizeof(int));
+    }
+
+    //Iterating over all the elements To WRITE
+    for (int i =0; i <3 ; i++)
+    {
+
+        for (int j=0 ; j<4 ; j++)
+        {
+            Array_2D[i][j] = i*j;
+        }
+    }
+
+    //Iterating over all the elements To READ
+    for (int i =0; i <3 ; i++)
+    {
+
+        for (int j=0 ; j<4 ; j++)
+        {
+            printf( "%d" , Array_2D[i][j]);
+        }
+    }
+    // Don't forget to free the allocated memory
+    for (int i = 0; i < 3; i++)
+    {
+        free(Array_2D[i]);
     }
 }
 
