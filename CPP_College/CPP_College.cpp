e4334a5b3910e81e5aa74261be2736290c74e289
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-10-28 13:11:54 +0530

    minor changes

--- CPP_College/CPP_College.cpp
+++ CPP_College/CPP_College.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 
+/*
+void Iterating_All_The_Elements(int*Array [3] , int Row , int Columns)
+{
+    for(int i =0 ; i < Row ; i++)
+    {
 
-
+        for ( int j =0; j< Columns;j++)
+        {
+            int Element = Array[i][j];
+        }
+    }
+}*/
 
 int main(int argc, char* argv[])
 {
+
+    
     return 0;
+
+
 }
