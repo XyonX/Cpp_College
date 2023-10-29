03d8c9a5bbb22c136d1848713db4ba1ce96cc052
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-10-29 09:46:45 +0530

    Added Array Linear Search function

---
+++ CPP_College/Array_Operations.cpp
@@ -0,0 +1,29 @@
+﻿
+
+int main ()
+
+{
+    int Demo_Array [10];
+
+    int ArraySize = sizeof(Demo_Array)/Demo_Array[0];
+
+    
+    //Linear Search
+    int LookupElement = 3;
+    for (int i = 0 ; i <ArraySize; i++ )
+    {
+        if(i == LookupElement)
+        {
+            return i;
+        }
+        else
+        {
+            return -1;
+        }
+            
+    }
+
+    //
+
+
+}
