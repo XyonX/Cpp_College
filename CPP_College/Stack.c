c395d0aff13ef5235adf84e72af29cda78a155f6
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-10-30 21:19:17 +0530

    Added stack class

---
+++ CPP_College/Stack.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#define MAX_SIZE 10
+struct Stack
+{
+    int Data [MAX_SIZE];
+    int Top = -1;
+};
+
+int Is_Empty (struct Stack*stack)
+{
+    return stack->Top ==-1;
+}
+int Is_Full (struct Stack*stack)
+{
+    return stack->Top == MAX_SIZE;
+}
+
+void Push (struct Stack*stack , int value)
+{
+    stack->Data[++stack->Top] =value;
+}
+int Pop ( struct Stack*stack)
+{
+    return stack->Data[stack->Top];
+}
