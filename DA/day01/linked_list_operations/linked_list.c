085882ee30394af4c1164c5afcc7618c5ea31909
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2024-02-04 23:58:59 +0530

    added linked list

--- DA/day01/linked_list_operations/linked_list.c
+++ DA/day01/linked_list_operations/linked_list.c
@@ -1,25 +1,65 @@
 #include<stdio.h>
 #include<stdlib.h>
-typedef struct node node;
 
-struct node
+typedef struct node
 {
-    int data;
-    node*next;
-};
+    void* data;
+    struct node*next;
+}LList;
 
-void insert(node**head)
+void insert(LList* pLList, void* data, int pos)
 {
+    if (pLList == NULL) {
+        return;
+    }
 
+    LList*newnode = (LList*)malloc(sizeof(LList));
+    if (newnode == NULL) {
+        return;
+    }
+
+    newnode->data=data;
+    newnode->next=NULL;
+
+    if(pos==0)
+    {
+        newnode->next=pLList;
+        pLList = newnode;
+    }
+    else if (pos>0)
+    {
+        int count=0;
+        LList*temp =pLList;
+        while (count <pos-1 && temp!=NULL)
+        {
+            temp=temp->next;
+            count++;
+        }
+        newnode->next=temp->next;
+        temp->next=newnode;
+    }
+    else if(pos==-1)
+    {
+        LList*temp=pLList;
+        while(temp->next!= NULL)
+        {
+            temp=temp->next;
+        }
+        temp ->next=newnode;
+    }
+    else
+    {
+        return ;
+    }
+    
 }
-void create(node**head)
+void create(LList**ppLLIST)
 {
-    *head=NULL;
+    *ppLLIST=NULL;
 }
-
-int status(node**input)
+int status(LList**ppLLIST)
 {
-    node*head=*input;
+    LList*head=*ppLLIST;
     if(head==NULL)
     {
         printf("The list is empty!!");
@@ -28,16 +68,16 @@ int status(node**input)
     printf("The list is not empty");
     return 1;
 }
-int destroy(node**input)
+int destroy(LList**input)
 {
-    node*head=*input;
+    LList*head=*input;
     if(head==NULL)
     {
         printf("Input list is already empty");
         return 0;
     }
-    node*current=head;
-    node*next;
+    LList*current=head;
+    LList*next;
 
     while(current != NULL)
     {
@@ -48,11 +88,48 @@ int destroy(node**input)
     head=NULL;
     return 1;
 }
+void removee (LList *pLList, int pos)
+{
+    int count=0;
+    LList*temp=pLList;
+    while (count<pos-1)
+    {
+        temp=temp->next;
+        count++;
+    }
+    temp->next=temp->next->next;
+    
+}
+void print(LList*pLList)
+{
+    if(pLList==NULL)
+    {
+        printf("The linked list is alredy empty");
+
+    }
+    LList*temp =pLList;
+    while (temp)
+    {
+        printf("%d \n",temp->data);
+    }
+    
+}
+void printelem(void*elem)
+{
+    printf("%d",*((int*)elem));
+}
+
 int main()
 {
-    node*head;
-    create(&head);
-    status(&head);
+    LList *pList1, *pList2;
+    create(&pList1); 
+    create(&pList2); 
+    int a = 10, b = 20,c = 30;
+    insert(pList1, &a, 0); 
+    insert(pList1, &b, -1); 
+    insert(pList2, &c, 0);
+    print((pList1));
+
 
 
 }
